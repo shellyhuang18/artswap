69ca3bb51dc312a3ec932b0b73a4e19f03cc2bfc
shellyhuang18 <shellyhuang81@gmail.com>
2017-11-04 13:16:23 -0400

    Check user and thread model please

--- models/thread.js
+++ models/thread.js
@@ -1,23 +1,23 @@
-//ENUM DEFINITION
-//====================================================
-var difficulty = {
-    BEGINNER: { value: 1, name: "Beginner" },
-    INTERMEDIATE: { value: 2, name: "Intermediate" },
-    PROFESSIONAL: { value: 3, name: "Professional" }
-};
-
-var purpose = {
-    COLLAB: { value: 1, name: "Collaberation" },
-    HIRE: { value: 2, name: "Hiring" }
-}
-//====================================================
-
 module.exports = (sequelize, DataTypes) => {
     var Thread = sequelize.define('Thread', {
-        title: DataTypes.STRING,
-        description: DataTypes.TEXT,
-        difficulty: DataTypes.difficulty,
-        purpose: DataTypes.purpose
+        title: {
+            allownNull: false,
+            type: DataTypes.STRING
+        },
+        description: {
+            allownNull: false,
+            type: DataTypes.TEXT
+        },
+        difficulty: {
+            allownNull: false,
+            type: DataTypes.ENUM,
+            values: ['Beginner', 'Intermediate', 'Professional']
+        },
+        purpose: {
+            allownNull: false,
+            type: DataTypes.ENUM,
+            values: ['Collab', 'Hiring']
+        }
     });
     Thread.associate = (models) => {
         //associate thread to user currently logged in 
--- models/user.js
+++ models/user.js
@@ -1,9 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
     var User = sequelize.define('User', {
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        email: DataTypes.STRING
-
+        userName: {
+        	allowNull: false,
+        	type: DataTypes.STRING,
+        	validate: {
+        		isAlphanumeric: true
+        	}
+        },
+        firstName: {
+        	allowNull: false,
+        	type: DataTypes.STRING
+        },
+        lastName: {
+        	allowNull: false,
+        	type: DataTypes.STRING
+        },
+        password: {
+        	allowNull: false,
+        	type: DataTypes.VIRTUAL
+        },
+        email: {
+        	allowNull: false,
+        	type: DataTypes.STRING
+        }
     });
     return User;
 }
