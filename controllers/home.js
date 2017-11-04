4ee54e7ab36e6cf4df06efe96a8e813911765145
shellyhuang18 <shellyhuang81@gmail.com>
2017-11-04 13:25:52 -0400

    oops forgot this one

--- controllers/home.js
+++ controllers/home.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const models = require('../models');
 
-const router = express.Router();
 
-// Allows user to create a thread 
-router.post('/thread', (req, res) => {
-  res.json({
-    msg: "Successful POST to '/' route"
-  });
-});
+module.exports = {
+  registerRouter() {
+    const router = express.Router();
 
+    router.post('/thread', this.create);
 
-module.exports = router;
+    return router;
+  },
+
+  // Allows user to create a thread 
+  create(req, res){
+ 
+  }
+};
+
