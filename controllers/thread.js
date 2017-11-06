7d1dfb920953af62a9b47a57d9026727e0127f0b
shellyhuang18 <shellyhuang81@gmail.com>
2017-11-06 16:08:40 -0500

    Able to view individual thread- need to implement posts in thread

--- controllers/thread.js
+++ controllers/thread.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const models = require('../models');
+const getSlug = require('speakingurl');
 
 module.exports = {
   registerRouter() {
     const router = express.Router();
 
     router.get('/', this.index);
+    router.get('/:slug', this.display);
     router.post('/', this.create);
     router.put('/', this.edit);
     router.delete('/', this.remove);
@@ -14,16 +16,16 @@ module.exports = {
   },
 
   index(req, res){
-    models.Thread.findAll()
+    models.Thread.findAll({
+      order: [[ 'createdAt', 'DESC']]
+    })
       .then((threads) => {
         res.render('threads', {
           threads: threads
         });
       });
-
-
-    
   },
+
 
 
   // Allows user to post a response, iff thread doesn't belong to user
@@ -45,18 +47,47 @@ module.exports = {
 
     models.Thread.create({
       userId: req.user.id,
+      slug: getSlug(req.body.title.toLowerCase()),
       title: req.body.title,
       description: req.body.description,
       difficulty: "Beginner",
       purpose: "Collab"
-    }).then((post) => {
-      res.redirect('/thread');
+    }).then((thread) => {
+      res.redirect(`/thread/${thread.slug}`);
     }).catch(() => {
       res.json({
         msg: "You fucked up"
       })
     });
   },
+
+
+  //Displays individual thread
+  display(req, res){
+    models.Thread.findOne({
+      where: {
+        slug: req.params.slug
+      },
+    }).then((thread) => {
+      res.render('threads/single', { thread })
+    }).catch(() => {
+      res.redirect('/thread')
+    })
+
+    // models.Thread.findOne({
+    //   where: { 
+    //     slug: req.params.slug
+    //   },
+    //   include: [{ 
+    //     model: models.Thread 
+    //   }],
+    // }).then((thread) => {
+    //   res.render('threads/single', { thread })
+    // }).catch(() => {
+    //   res.redirect('/thread')
+    // })
+  },
+
 
   // Allows user to edit thread, iff thread belongs to user
   edit(req, res){
