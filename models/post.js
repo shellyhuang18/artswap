2ab8296eef64fc7438bc3e1f1f9a929c593055bd
shellyhuang18 <shellyhuang81@gmail.com>
2017-12-03 20:01:52 -0500

    Update post.js

--- models/post.js
+++ models/post.js
@@ -1,24 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define('post', {
-    slug: {
-      type: DataTypes.STRING,
-      unique: 'compositeIndex',
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      }
-      
-    },
-    title:{
-      type: DataTypes.STRING,
-      unique: 'compositeIndex',
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      }
-    },
-    
-    
     body: {
       type: DataTypes.STRING,
       allowNull: false,
